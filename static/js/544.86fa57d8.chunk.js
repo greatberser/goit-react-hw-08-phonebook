612a2013b838b8a600610e458a210bfb78303fc9
greatberser <130564895+greatberser@users.noreply.github.com>
2024-02-01 13:13:01 +0000

    Deploying to gh-pages from @ greatberser/goit-react-hw-08-phonebook@f85b610319d360adf2ee5928c106757d55a3c88e 🚀

---
+++ static/js/544.86fa57d8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[544],{9544:function(e,t,n){n.r(t),n.d(t,{default:function(){return r}});var i=n(3329),c={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function r(){return(0,i.jsx)("div",{style:c.container,children:(0,i.jsx)("h1",{style:c.title,children:"Welcome to Phonebook!"})})}}}]);
+//# sourceMappingURL=544.86fa57d8.chunk.js.map
