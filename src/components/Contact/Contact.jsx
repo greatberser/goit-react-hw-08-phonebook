17ddc5b9df74f248c2093d9aba4d8bc030ce83a9
Maks <maks135254@gmail.com>
2024-02-02 17:26:15 +0200

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,10 @@
 import css from './Contact.module.css'
 
-export const Contact = ({contact: {name, phone}, onClickDelBtn}) => {
+export const Contact = ({contact: {name, number}, onClickDelBtn}) => {
     return (
         <li className={css.contactLine}>
             <span className={css.contactName}>{`${name}`}</span>
-            <span className={css.contactNumber}>{`${phone}`}</span>
+            <span className={css.contactNumber}>{`${number}`}</span>
             <button className={css.contactDelete} type="button" onClick={onClickDelBtn}>
                 Delete
             </button>
